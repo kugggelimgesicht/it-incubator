e4532c084d9b3255f38daa059cefc7f4b6bf20fb
Darya Wolfson <w0lfs0hnds@gmail.com>
2021-01-30 21:12:22 +0300

    Updates

---
+++ precache-manifest.65296b78e2c37c13d79d5c5fe71a3f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e196423fef2c7e0282b2f1f9530038e",
+    "url": "/it-incubator/index.html"
+  },
+  {
+    "revision": "08f02ab3d75693b0d257",
+    "url": "/it-incubator/static/css/main.2062bf34.chunk.css"
+  },
+  {
+    "revision": "446d0cd76c9fe11f6f48",
+    "url": "/it-incubator/static/js/2.e89d95b6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/it-incubator/static/js/2.e89d95b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f02ab3d75693b0d257",
+    "url": "/it-incubator/static/js/main.8da55301.chunk.js"
+  },
+  {
+    "revision": "d9c5e49800f0e8c8bc7d",
+    "url": "/it-incubator/static/js/runtime-main.6978b11e.js"
+  }
+]);
